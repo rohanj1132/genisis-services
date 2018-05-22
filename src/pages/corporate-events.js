9965d06679c8e5d40d1132312794d2c1d99a2788
rohan jaiswal <rohan.jaiswal@amdocs.com>
2018-05-23 00:48:34 +0530

    Add corporate, social and wedding event pages
    Also added link from events page

---
+++ src/pages/corporate-events.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+
+const CorporateEventsPage = () => (
+    <div>
+        <PageTitle title='Corporate Events' />
+        <div className='container'>
+            <p>
+                We’ll Bring Your Event to Life!
+                Our corporate customers never stop to stun us. Throughout the years, we have had the benefit to work with some skilled and creative individuals. We're fortunate to have assembled enduring connections and are regarded to work with these people consistently.
+                We have collaborated on amazing events, including:
+                •	Fundraisers
+                •	Stunning Galas
+                •	Award and Scholarship Banquets
+                •	Premier Events
+                •	Grand Openings
+                •	Open Houses
+                •	Haute Holiday Parties
+                •	Breakfast Meetings
+                •	Corporate Luncheons
+                •	Cocktail Receptions
+                •	Company Picnics
+                •	Summer BBQs
+                •	Carnivals
+                •	Concerts…the list goes on.
+                What awesome thoughts have you been supposing about? We'd love to encourage convey those plans to reality. Our occasion experts will outline occasions suited to address your issues. We'll work with you to upgrade space and measurement, to consolidate your Company picture and brand, to add amazing components to wow your visitors, to mirror the occasion message, and to discover answers for fit your financial plan.
+                Genisis Services needs your occasion to be a win and our group of authorities will be here for you always. Depend on us to create a consistent and critical occasion where you, the host, can kick back and have a good time.
+            </p>
+        </div>
+    </div>
+)
+
+export default CorporateEventsPage;
