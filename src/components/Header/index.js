40d0961996049abcb992e455ffdf22f58c7622f0
rohan jaiswal <rohan.jaiswal@amdocs.com>
2018-05-22 20:28:12 +0530

    Add content to about us page
    Added bootstrap jumbotron to display page header, quoted important text and added values in a list

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,52 +4,54 @@ import Navbar from '../Navigation/index'
 import logo from '../../../content/logo.jpg'
 
 export default class Header extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      showNav: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            showNav: false
+        }
+        this.onNavBarTogglerClick = this.onNavBarTogglerClick.bind(this);
     }
-    this.onNavBarTogglerClick = this.onNavBarTogglerClick.bind(this);
-  }
 
-  onNavBarTogglerClick () {
-    this.setState((prevState) => {
-      return {
-        showNav: !prevState.showNav
-      };
-    });
-  }
+    onNavBarTogglerClick() {
+        this.setState((prevState) => {
+            return {
+                showNav: !prevState.showNav
+            };
+        });
+    }
 
-  render () {
-    return (
-      <header className='navbar navbar-expand-lg box-shadow navbar-light'>
-        <Link
-          to='/'
-          className='navbar-brand'
-        >
-          <img
-            src={logo}
-            alt='genisis services logo'
-            style={{
-              height: '140px'
-            }} />
-        </Link>
-        <button 
-          className={`navbar-toggler ${this.state.showNav ? '': 'collapsed'}`}
-          type='button' 
-          onClick={this.onNavBarTogglerClick}
-          data-toggle='collapse' 
-          data-target='#navbarSupportedContent' 
-          aria-controls='navbarSupportedContent' 
-          aria-expanded='false' 
-          aria-label='Toggle navigation'>
-          <span className='navbar-toggler-icon'></span>
-        </button>
+    render() {
+        return (
+            <header className='navbar navbar-expand-lg box-shadow navbar-light'>
+                <div className='container'>
+                    <Link
+                        to='/'
+                        className='navbar-brand'
+                    >
+                        <img
+                            src={logo}
+                            alt='genisis services logo'
+                            style={{
+                                height: '140px'
+                            }} />
+                    </Link>
+                    <button
+                        className={`navbar-toggler ${this.state.showNav ? '' : 'collapsed'}`}
+                        type='button'
+                        onClick={this.onNavBarTogglerClick}
+                        data-toggle='collapse'
+                        data-target='#navbarSupportedContent'
+                        aria-controls='navbarSupportedContent'
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'>
+                        <span className='navbar-toggler-icon'></span>
+                    </button>
 
-        <div className={`collapse navbar-collapse ${this.state.showNav ? 'show': ''}`} id='navbarSupportedContent'>
-          <Navbar />
-        </div>
-      </header>
-    );
-  }
+                    <div className={`collapse navbar-collapse ${this.state.showNav ? 'show' : ''}`} id='navbarSupportedContent'>
+                        <Navbar />
+                    </div>
+                </div>
+            </header>
+        );
+    }
 }
